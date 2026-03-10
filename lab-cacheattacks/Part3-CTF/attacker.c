a9eae67e665473ac16eac2aee4fcbacfe82f0311
Ethan Hernandez <111538924+eah1206@users.noreply.github.com>
2026-03-09 22:19:02 -0400

    Volatile removal

--- lab-cacheattacks/Part3-CTF/attacker.c
+++ lab-cacheattacks/Part3-CTF/attacker.c
@@ -1,12 +1,29 @@
 #include "util.h"
 // mman library to be used for hugepage allocations (e.g. mmap or posix_memalign only)
 #include <sys/mman.h>
+#define BUFF_SIZE (1<<21)
 
 int main(int argc, char const *argv[]) {
     int flag = -1;
 
     // Put your capture-the-flag code here
-    
+
+    void *buf = mmap(NULL, BUFF_SIZE,
+                     PROT_READ | PROT_WRITE,
+                     MAP_POPULATE | MAP_ANONYMOUS | MAP_PRIVATE | MAP_HUGETLB,
+                     -1, 0);
+
+    // Check for allocation failure
+    if (buf == (void *)-1) {
+        perror("mmap() error");
+        exit(EXIT_FAILURE);
+    }
+
+    // Prime all cache sets in the buffer
+
+    // In a loop, probe all cache sets, checking for high latency hits
+
+    // Yeah
 
     printf("Flag: %d\n", flag);
     return 0;
