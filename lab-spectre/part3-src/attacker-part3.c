3b2ee92ec2106390e5cd4fad2b386db3566db122
Ethan Hernandez <111538924+eah1206@users.noreply.github.com>
2026-04-08 01:27:48 -0400

    Update attacker-part3.c

--- lab-spectre/part3-src/attacker-part3.c
+++ lab-spectre/part3-src/attacker-part3.c
@@ -58,9 +58,9 @@ int run_attacker(int kernel_fd, char *shared_memory) {
 
         uint64_t shortest_access = (uint64_t) -1;
         int best_i = -1;
-        for (int trial = 0; trial < 500; trial++) {
+        for (int trial = 0; trial < 100; trial++) {
             // Train Branch Predictor
-            for (int train = 0; train < 100; train++) {
+            for (int train = 0; train < 30; train++) {
                 call_kernel_part3(kernel_fd, shared_memory, train % 4);
             }
 
